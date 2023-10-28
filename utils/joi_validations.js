251c99c4499a63e7e3afb9a916b33e4e192ffc3a
ayushmittal007 <ayushagrawal102004@gmail.com>
2023-10-28 19:50:35 +0530

    added condition for password length

--- utils/joi_validations.js
+++ utils/joi_validations.js
@@ -3,7 +3,7 @@ const Joi = require("joi");
 const authSchema = Joi.object({
   username: Joi.string().required(),
   email: Joi.string().email().required(),
-  password: Joi.string().alphanum().required(),
+  password: Joi.string().alphanum().min(6).required(),
 });
 
 module.exports = {
--- utils/validator_for_change_pass.js
+++ utils/validator_for_change_pass.js
@@ -2,7 +2,7 @@ const Joi = require("joi");
 
 const newSchema = Joi.object({
   email: Joi.string().email().required(),
-  newPassword: Joi.string().alphanum().required(),
+  newPassword: Joi.string().alphanum().min(6).required(),
 });
 
 module.exports = {
