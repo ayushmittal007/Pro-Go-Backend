d957ca2f4b4cbf49f82d95102160aac659cbe446
ayushmittal007 <ayushagrawal102004@gmail.com>
2023-11-01 23:29:38 +0530

    changed email format

--- utils/send_mail.js
+++ utils/send_mail.js
@@ -7,8 +7,20 @@ const sendmail = async (email,otp) => {
         to: email,
         subject: "Verification code",
         html: 
-        ` <p style="margin-bottom: 30px;">Please enter this OTP to proceed further</p>
-          <h1 style="font-size: 40px; letter-spacing: 2px; text-align:center; color:green;">${otp}</h1>`,
+        ` <p style="margin-bottom: 30px;"> We are thrilled to have you on the Pro-Go community! To verify your email
+        address and complete the registration process, please use the following
+        One-Time Password (OTP) code:
+          </p>
+          <h1 style="font-size: 40px; letter-spacing: 2px; text-align:center; color:green;">${otp}</h1>
+          <p>
+          If you didn't request this OTP or have any concerns about your Pro-Go
+          account, please ignore this email.
+        </p>
+        <p>
+          Thank you for choosing Pro-Go, and we look forward to helping you stay
+          organized and productive!
+        </p>`
+      ,
     }
 
     const transporter = nodemailer.createTransport({
