49919496318cbd5f56385ae935164d312bf7f312
ayushmittal007 <ayushagrawal102004@gmail.com>
2023-11-12 09:55:17 +0530

    optimised profile photo apis

--- model/index.js
+++ model/index.js
@@ -1,6 +1,5 @@
 module.exports.User = require("./user");
 module.exports.Otp = require("./otp");
-module.exports.photo = require("./profile_photo");
 module.exports.Board = require("./board");
 module.exports.List = require("./list");
 module.exports.Card = require("./card");
