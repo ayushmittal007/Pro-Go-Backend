96115a5d7b1f79ddf52f58f334077b5560229655
ayushmittal007 <ayushagrawal102004@gmail.com>
2023-11-22 03:10:42 +0530

    debug

--- Routes/board-routes.js
+++ Routes/board-routes.js
@@ -6,11 +6,11 @@ const auth = require("../middlewares/auth")
 boardRouter.get("/", auth , boardController.getAll);
 boardRouter.post("/add", auth, boardController.addBoard);
 boardRouter.put("/:id/update", auth , boardController.updateBoardById);
-boardRouter.get("/:id", auth , boardController.getBoardById);
+boardRouter.get("/:id" , boardController.getBoardById);
 boardRouter.get("/:id/lists", auth , boardController.getLists);
 boardRouter.get("/:id/cards", auth , boardController.getCards);
 boardRouter.delete("/:id/delete", auth , boardController.deleteBoard);
-boardRouter.post("/:id/addMember" , boardController.addMember);
+boardRouter.post("/:id/addMember", auth , boardController.addMember);
 boardRouter.get("/:id/getMembers", auth , boardController.getAllMemberInTheBoard);
 
 module.exports = boardRouter;
