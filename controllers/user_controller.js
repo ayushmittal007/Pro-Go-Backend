c88de99afe868cc0a02fa498635960c4243e1059
ayushmittal007 <ayushagrawal102004@gmail.com>
2023-11-24 19:40:54 +0530

    progress bar api

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,4 +1,3 @@
-const mongoose = require('mongoose');
 const { Board, List, Card , user } = require('../model');
 const { ErrorHandler } = require('../middlewares/errorHandling');
 const { emailSchema , recentlyViewedSchema , recentlyWorkedSchema } = require("../utils/joi_validations");
@@ -27,110 +26,113 @@ const uploadProfilePhoto = async (req, res, next) => {
     }
   }
 
-  const getPhotoUrl = async (req, res, next) => {
-    try {
-      const existing = await user.findOne({email : req.user.email});
-      if(existing.photoUrl == null){
-        return next (new ErrorHandler(400 , "No photo found"));
-      }
-      else {
-        res.json({
-          success: true,
-          photoUrl : existing.photoUrl
-        });
-      }
-    } catch (e) {
-      next(e);
+const getPhotoUrl = async (req, res, next) => {
+  try {
+    const existing = await user.findOne({email : req.user.email});
+    if(existing.photoUrl == null){
+      return next (new ErrorHandler(400 , "No photo found"));
     }
-  }
-
-  const addUserDetails = async (req, res, next) => {
-    try{
-      const existing = await user.findOne({email : req.user.email});
-      console.log(existing);
-      if(existing == null){
-        return next (new ErrorHandler(400 , "No user found"));
-      }else {
-        if(req.body.fullName != null){
-          existing.fullName = req.body.fullName;
-        }
-        if(req.body.jobTitle != null){
-          existing.jobTitle = req.body.jobTitle;
-        }
-        if(req.body.department != null){
-          existing.department = req.body.department;
-        }
-        if(req.body.organisation != null){
-          existing.organisation = req.body.organisation;
-        }
-        if(req.body.basedIn != null){
-          existing.basedIn = req.body.basedIn;
-        }
-        if(req.body.region != null){
-          existing.region = req.body.region;
-        }
-        await existing.save();
-        res.json({
-          success: true,
-          message : "User details added successfully"
-        });
-      }
-    }catch(e){
-      next(e);
-    }   
-  }
-
-  const getUserDetails = async (req, res, next) => {
-    try {
-      const existing = await user.findOne({email : req.user.email});
-      if(existing == null){
-       return next (new ErrorHandler(400 , "No user found"));
-      }
-      else {
-        res.json({
-          success: true,
-          user : existing
-        });
-      }
-    } catch (e) {
-      next(e);
-    }
-  }
-
-  const addWorkSpaceMember = async (req, res, next) => {
-    try{
-      const User = req.user;
-      const existing = await user.findOne({email : req.body.email});
-      if(existing == null){
-       return next (new ErrorHandler(400 , "No user found"));
-      }
-      else {
-        if(User.usersWorkSpcaeMember.includes(req.body.email)){
-          return next (new ErrorHandler(400 , "User already added"));
-        }
-        User.usersWorkSpcaeMember.push(req.body.email);
-        await User.save();
-        res.json({
-          success: true,
-          message : "User added successfully"
-        });
-      }
-    }catch(err){
-      next(err);
-    }
-  }
-
-  const getAllWorkSpaceMember = async (req, res, next) => {
-    try{
-      const User = req.user;
+    else {
       res.json({
         success: true,
-        usersWorkSpcaeMember : User.usersWorkSpcaeMember
+        photoUrl : existing.photoUrl
       });
-    }catch(err){
-      next(err);
     }
+  } catch (e) {
+    next(e);
   }
+}
+
+const addUserDetails = async (req, res, next) => {
+  try{
+    const existing = await user.findOne({email : req.user.email});
+    console.log(existing);
+    if(existing == null){
+      return next (new ErrorHandler(400 , "No user found"));
+    }else {
+      if(req.body.fullName != null){
+        existing.fullName = req.body.fullName;
+      }
+      if(req.body.jobTitle != null){
+        existing.jobTitle = req.body.jobTitle;
+      }
+      if(req.body.department != null){
+        existing.department = req.body.department;
+      }
+      if(req.body.organisation != null){
+        existing.organisation = req.body.organisation;
+      }
+      if(req.body.basedIn != null){
+        existing.basedIn = req.body.basedIn;
+      }
+      if(req.body.region != null){
+        existing.region = req.body.region;
+      }
+      await existing.save();
+      res.json({
+        success: true,
+        message : "User details added successfully"
+      });
+    }
+  }catch(e){
+    next(e);
+  }   
+}
+
+const getUserDetails = async (req, res, next) => {
+  try {
+    const existing = await user.findOne({email : req.user.email});
+    if(existing == null){
+      return next (new ErrorHandler(400 , "No user found"));
+    }
+    else {
+      res.json({
+        success: true,
+        user : existing
+      });
+    }
+  } catch (e) {
+    next(e);
+  }
+}
+
+const addWorkSpaceMember = async (req, res, next) => {
+  try{
+    const User = req.user;
+    const existing = await user.findOne({email : req.body.email});
+    if(existing == null){
+      return next (new ErrorHandler(400 , "No user found"));
+    }
+    else {
+      if(User.usersWorkSpcaeMember.includes(req.body.email)){
+        return next (new ErrorHandler(400 , "User already added"));
+      }
+      User.usersWorkSpcaeMember.push(req.body.email);
+      await User.save();
+      res.json({
+        success: true,
+        message : "User added successfully"
+      });
+    }
+  }catch(err){
+    next(err);
+  }
+}
+
+const getAllWorkSpaceMember = async (req, res, next) => {
+  try{
+    const User = req.user;
+    if(!User.usersWorkSpcaeMember){
+      return next (new ErrorHandler(400 , "No user found"));
+    }
+    res.json({
+      success: true,
+      usersWorkSpcaeMember : User.usersWorkSpcaeMember
+    });
+  }catch(err){
+    next(err);
+  }
+}
 
 const addRecentlyViewed = async (req, res, next) => { 
   try{
@@ -214,6 +216,22 @@ const search = async (req, res,next) => {
       next(error);
   }
 };
+
+const progress = async (req, res, next) => {
+  try {
+    const userId = req.user._id;
+    const doneCards = await Card.find({userId : userId , done : true});
+    const totalCards = await Card.find({ userId : userId });
+    const completed = doneCards.length;
+    const total = totalCards.length;
+    res.json({
+      success: true,
+      progress : (completed/total)*100
+    });
+  } catch (e) {
+    next(e);
+  }
+}
 
 module.exports = {
   uploadProfilePhoto,
