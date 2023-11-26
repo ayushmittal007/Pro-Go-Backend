69ed72ea97327dacb94360b6d02bd44e37b61527
ayushmittal007 <ayushagrawal102004@gmail.com>
2023-11-27 02:22:39 +0530

    removed views folder

--- Routes/payment-routes.js
+++ Routes/payment-routes.js
@@ -3,12 +3,7 @@ const paymentRouter = express.Router();
 const { paymentController } = require("../controllers");
 const auth = require("../middlewares/auth");
 
-paymentRouter.get("/", (req, res)=>{
-    res.render("makePayment")
-})
-
 paymentRouter.post("/createOrder" , auth , paymentController.createOrder)
-
 paymentRouter.post("/checkPayment", auth ,paymentController.checkPayment)
 
 module.exports = paymentRouter;
